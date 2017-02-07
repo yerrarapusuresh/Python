40d3e4ddb5e89d3ecb42fc33e6558082380aeed0
Suresh Yerrarapu <yerrarapusuresh@gmail.com>
2017-02-07 17:04:08 +0530

    It'll print current time

--- PythonIntro/Functions/break_time.py
+++ PythonIntro/Functions/break_time.py
@@ -2,7 +2,9 @@ import webbrowser
 import time 
 
 counter = 1
+
 while counter <= 3:
+	print ("current time is now "+time.ctime())
 	time.sleep(10)
 	webbrowser.open("https://www.youtube.com/watch?v=bhplCNECg9E")
 	counter += 1
