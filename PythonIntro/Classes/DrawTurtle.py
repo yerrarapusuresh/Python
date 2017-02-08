26c17219d661ee8b0dec526c93af621817bcef52
Suresh Yerrarapu <yerrarapusuresh@gmail.com>
2017-02-08 17:46:49 +0530

    wrote program for draw turtle

---
+++ PythonIntro/Classes/DrawTurtle.py
@@ -0,0 +1,14 @@
+import turtle
+
+def draw_squre():
+	pinkey = turtle.Turtle()
+	pinkey.forward(20)
+	pinkey.right(90)
+	pinkey.forward(20)
+	pinkey.right(90)
+	pinkey.forward(20)
+	pinkey.right(90)
+	pinkey.forward(20)
+	pinkey.right(90)
+
+draw_squre()
